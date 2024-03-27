bfb3dab24306af806ed67df3886582c669982af2
furkanpece <furkanpece61furkanpece@gmail.com>
2024-03-27 22:25:04 +0300

    s

---
+++ doktor_ekle.php
@@ -0,0 +1,130 @@
+<?php
+include("inc/header.php"); 
+?>
+      
+
+    <section class="pt-120 pb-80">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-4">
+            <aside class="sidebar has-marign-right">
+              <div class="widget widget-member">
+                <div class="member mb-0">
+				<div class="member__img"><img src="../assest/img/card/2.png.webp" alt="Doç. Dr. Muzaffer AL">
+      </div>
+        <div class="member__info"><h5 class="member__name">
+          
+        <input type="text" id="inputName" class="form-control" name="gorev_projeadi"  placeholder="İsim Soyisim">
+      
+   </h5>
+        <p class="member__job">
+        <input type="text" id="inputName" class="form-control" name="gorev_projeadi"  placeholder="Pozisyon">
+        </p>
+      </div>
+                </div>
+              </div>
+			  
+              <div class="widget widget-help bg-overlay bg-overlay-primary-gradient bg-img" >
+                
+                <div class="widget-content">
+                  <div class="widget__icon">
+                  <i class="fa-solid fa-phone"></i>
+                  </div>
+                  <h4 class="widget__title">Call Us Now</h4>
+                  <p class="widget__desc">Please feel free to contact our friendly reception staff with any general or medical enquiry.</p>
+                  <a href="tel:905354914556" class="phone__number">
+                    <!--<i class="icon-phone"></i>--><span>+90 535 491 45 56</span>
+                  </a>
+                </div><!-- /.widget-content -->
+              </div><!-- /.widget-help -->
+
+
+			  
+            </aside><!-- /.sidebar -->
+          </div><!-- /.col-lg-4 -->
+		  
+		  
+          <div class="col-sm-12 col-md-12 col-lg-8">
+            <ul class="details-list list-unstyled mb-60"><li>
+					<h5 class="details__title">Hospital </h5>
+					<div class="details__content">
+					  <p class="mb-0">
+            <input type="text" id="inputName" class="form-control" name="gorev_projeadi"  placeholder="Pozisyon">
+            </p>
+					</div>
+				  </li>
+          <li>
+					<h5 class="details__title">Expertise</h5>
+					<div class="details__content">
+					  <p class="mb-0">
+            <input type="text" id="inputName" class="form-control" name="gorev_projeadi"  placeholder="Pozisyon">
+            </p>
+					</div>
+				  </li>
+          <li>
+						<h5 class="details__title">Academic Title</h5>
+						<div class="details__content">
+						  <p class="mb-0">
+              <input type="text" id="inputName" class="form-control" name="gorev_projeadi"  placeholder="Pozisyon">
+              </p>
+						</div>
+					  </li>
+            <li>
+					<h5 class="details__title">Areas of Expertise</h5>
+					<div class="details__content">
+					  <ul class="list-items list-items-layout2 list-unstyled mb-0">
+              <li>Metabolic and bariatric revision surgery</li>
+              <li>Metabolic Surgery for Type 2 Diabetes</li>
+              <li>Gastric Balloon</li>
+              <li>Gastric Botox</li>
+              <li>Gastric Bypass</li>
+              <li>Laparoscopic Sleeve Gastrectomy</li>
+              <li>Obesity and Bariatric Surgery</li>
+            </ul>
+					</div>
+				  </li></ul><div class="text-block mb-50">
+					  <h5 class="text-block__title">Areas Of Expertise</h5>					  
+					  <p class="text-block__desc mb-20"></p>
+            <p>Intestinal cancers Stomach Cancer General Surgery</p>
+            <p>Metabolic and bariatric revision surgery</p>
+            <p>Metabolic Surgery for Type 2 Diabetes</p>
+            <p>Gastric Balloon</p>
+            <p>Gastric Botox</p>
+            <p>Gastric Bypass Obesity and Bariatric Surgery</p>
+<p></p>
+					</div><div class="text-block mb-50">
+					  <h5 class="text-block__title">Training</h5>					  
+					  <p class="text-block__desc mb-20"></p>
+            <p>Ondokuz Mayıs University Faculty of Medicine (1991)</p>
+            <p>K.T.Ü Medical Faculty Farabi Hospital (Specialization 1999)</p>
+            <p>Istanbul University, Çapa Medical Faculty (Laparoscopic Surgery Education, 2002)</p>
+            <p>Istanbul University Faculty of Medicine (Laparoscopic Antirefluous Surgery Education, 2006)</p>
+            <p>İstanbul University Cerrahpaşa Medical Faculty (General Surgery Clinic, Advance Laparoscopic Surgery, Laparoscopic Bariatric Surgery, Morbid Obesity and Oncologic Cancer Surgery Education, 2010)</p>
+            <p></p>
+					</div><div class="text-block mb-50">
+					  <h5 class="text-block__title">Experience</h5>					  
+					  <p class="text-block__desc mb-20"></p><p>IRCAD University Strasbourg France 2014 Robotic and Laparoscopic Bariatric Surgery Trainings</p>
+
+<p>University of Sao Paulo Brazil 2015 Pure Laparoscopic Metabolic Surgery Training</p>
+<p></p>
+					</div><div class="text-block mb-50">
+					  <h5 class="text-block__title">Vocational Training, Courses and Conferences</h5>					  
+					  <p class="text-block__desc mb-20"></p>
+            <textarea id="inputDescription" class="form-control" rows="4" name="gorev_aciklama" placeholder="Pozisyon"></textarea>
+					</div>
+          <div class="text-block mb-50">
+					  <h5 class="text-block__title">Professional Memberships</h5>					  
+					  <p class="text-block__desc mb-20"></p>
+            <textarea id="inputDescription" class="form-control" rows="4" name="gorev_aciklama" placeholder="Pozisyon"></textarea>
+					</div>			
+			
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+      <?php
+include("inc/footer.php"); 
+?>
--- login.php
+++ login.php
@@ -1,126 +1,76 @@
 <?php
-include("config/ayar.php"); 
+include("../assest/inc/data.php"); 
 ob_start();
 session_start();
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+  <!-- Required meta tags -->
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>İş Yönetim | Büyük Anadolu Hastanesi</title>
-  <link rel="icon" href="img/icon.png" >
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="theme/plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="theme/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="theme/dist/css/adminlte.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Büyük Anadolu Hastanesi</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="vendors/feather/feather.css">
+  <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="css/vertical-layout-light/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="images/favicon.png" />
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    
-    <div class="card-header text-center">
-      <span style="color:#283947;"><b style="color:#f96a44;">BAH</b> Büyük Anadolu Hastanesi</span><br><a href="" class="h1"><b>İş </b>Yönetim</a>
-           
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Giriş Yap</p>
 
-      <form action="#" method="post">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Kullanıcı Adı" name="username">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo">
+                <img src="../assest/img/bah.png" alt="logo">
+              </div>
+              <h4>Devam etmek için oturum açın.</h4>
+              
+              <form class="pt-3">
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-lg"  placeholder="Kullanıcı Adı" name="username">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg" placeholder="Şifre" name="password">
+                </div>
+                <div class="mt-3">
+                  <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" >Giriş Yap</button>
+                </div>
+
+              </form>
+
             </div>
           </div>
         </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Şifre" name="password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <!-- /.col -->
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary btn-block">Giriş Yap</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-<?php
-$value = "123456789abcdef";
-$end    = substr($value, -4 ,4); // Sondan 10 Karakteri Alıyoruz.
-$start  = substr($value, 0 ,0); // tamamını alıyoruz.
-$result = str_replace([$start, $end], null, $value); // Metnin İçinden Eşleşenleri Siliyoruz.
-
-
-
-
-
-
-if ($_POST) {
-$username = addslashes(strip_tags($_POST["username"]));
-$password = addslashes(strip_tags($_POST["password"]));
-
-
-$salt = "18811938]au";
-$md5 = md5($password); 
-$guvenlisifre = md5($md5.$salt);
-
- if (!$username || !$password) {
-   echo '<br><div class="alert alert-danger alert-dismissible">
-                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                   Boş Alan Bırakmayın.
-                </div>';
- }
- else {
-$girisyap=$db->prepare("SELECT * from user WHERE user_name='$username' and user_password='$guvenlisifre'");
-$girisyap->execute();
-$giris_row=$girisyap->fetch(PDO::FETCH_ASSOC);
-
-   if ($girisyap->rowcount()) {
-     $_SESSION["oturum"] = "yes";
-     $_SESSION["username"]= $giris_row["user_name"];
-     $_SESSION["user_id"] = $giris_row["user_id"];
-     header("location:index.php?sayfa=main");
-   }
-   else{
-   echo'<br><div class="alert alert-danger alert-dismissible">
-                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                   Kullanıcı adı veya şifreniz hatalı
-                </div>';
-   }
- }
- 
-}
-
-?>
+      </div>
+      <!-- content-wrapper ends -->
     </div>
-    <!-- /.card-body -->
+    <!-- page-body-wrapper ends -->
   </div>
-  <!-- /.card -->
-</div>
-<!-- /.login-box -->
-
-<!-- jQuery -->
-<script src="theme/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="theme/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="theme/dist/js/adminlte.min.js"></script>
-<script type='text/javascript' src='js/assets/js/art.js'></script>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="js/off-canvas.js"></script>
+  <script src="js/hoverable-collapse.js"></script>
+  <script src="js/template.js"></script>
+  <script src="js/settings.js"></script>
+  <script src="js/todolist.js"></script>
+  <!-- endinject -->
 </body>
+
 </html>
